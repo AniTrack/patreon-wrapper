831c087cf36bd1c40aba643114256dbc83d70714
ZSPFX <gunduzkamen@gmail.com>
2022-03-13 00:15:32 +0700

    Build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,35 @@ declare type Auth = {
     AccessToken: string;
     CampaignID: string;
 };
+declare type PatronType = {
+    displayId: string;
+    displayName: string;
+    emailAddress: string;
+    isFollower: boolean;
+    subscription: {
+        note: string;
+        currentEntitled: {
+            status: string;
+            tierId: number;
+            cents: number;
+            willPayCents: number;
+            lifetimeCents: number;
+            firstCharge: Date;
+            nextCharge: Date;
+            lastCharge: Date;
+        };
+    };
+    mediaConnection: {
+        patreon: {
+            id: string;
+            url: string;
+        };
+        discord: {
+            id: string;
+            url: string;
+        };
+    };
+};
 export declare class Patreon {
     private static _URL;
     private static _AccessToken;
@@ -9,7 +38,7 @@ export declare class Patreon {
     static Authorization(AuthInformation: Auth): void;
     private static FetchAPI;
     private static CleanQueryURL;
-    static FetchPatrons(filter?: string[]): Promise<any[]>;
+    static FetchPatrons(filter?: string[]): Promise<PatronType[]>;
 }
 export {};
 //# sourceMappingURL=index.d.ts.map
